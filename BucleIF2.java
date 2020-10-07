050b4146362806126414f0d97c9745ceb9d67dea
barrospivan <barrospivan@gmail.com>
2020-10-07 09:05:13 +0200

    pr3

---
+++ BucleIF2.java
@@ -0,0 +1,27 @@
+
+public class BucleIF2 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=17;
+		
+		//OPERADOR LÓGICO AND
+		
+		if(num>=10 && num<15) {
+			System.out.println("El número es mayor o igual que 10 y menor que 15");
+		}else {
+			System.out.println("El numero es menor que 10");
+		}
+		
+		//OPERADOR LÓGICO OR
+		
+		if(num>=10 || num<15) {
+			System.out.println("El número es mayor o igual que 10 o menor que 15");
+		}else {
+			System.out.println("El numero es menor que 10");
+		}
+	}
+
+}
